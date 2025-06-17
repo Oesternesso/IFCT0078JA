817be4eff6c7ff7254fec95b2ed31afaea264153
Oesternesso <juancarlosjuancarlosvalle@gmail.com>
2025-06-17 12:35:40 +0200

    trasteo aun mas

--- src/assets/js/custom.v1.5.0.js
+++ src/assets/js/custom.v1.5.0.js
@@ -4,14 +4,14 @@ const farenheit = Number(prompt("Introduzca grados Farenheit para convertirlos a
 
 function celsiusAFahrenheit(){
    let F = (celsius * 9/5) + 32;
-   return console.log("La conversión Celsius a Fahrenheit es de" + F + "Fº");
+   return console.log("La conversión Celsius a Fahrenheit es" + F + "Fº");
    
 }
 celsiusAFahrenheit();
 
 function farenheitACelsius(){
     let C = (farenheit - 32) * 5/9;
-    return console.log("La conversión Fahrenheit a Celsius es de" + C + "Cº");
+    return console.log("La conversión Fahrenheit a Celsius es de"+ C + "Cº");
 
 } 
 farenheitACelsius();
